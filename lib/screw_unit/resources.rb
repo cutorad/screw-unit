fbb0dfdd3038ec74bce24d68f6498f97fea3adee
Michael Buffington <michael.buffington@gmail.com>
2009-07-28 13:08:57 -0700

    hacked and slashed until I got things working

--- lib/screw_unit/resources.rb
+++
@@ -1,8 +0,0 @@
-dir = File.dirname(__FILE__)
-require "#{dir}/resources/file"
-require "#{dir}/resources/dir"
-require "#{dir}/resources/root"
-require "#{dir}/resources/spec_dir"
-require "#{dir}/resources/spec_suite"
-require "#{dir}/resources/file_not_found"
-require "#{dir}/resources/suite_completion"
