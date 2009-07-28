55e9093348c96787856b1d08b5e635ef4f48144d
Michael Buffington <michael.buffington@gmail.com>
2009-07-28 16:29:26 -0700

    removing all but the essentials

--- spec/screw_unit_spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-require "spec"
-
-dir = File.dirname(__FILE__)
-require File.expand_path("#{dir}/../lib/screw_unit")
-
-Spec::Runner.configure do |config|
-  config.mock_with :rr
-end
